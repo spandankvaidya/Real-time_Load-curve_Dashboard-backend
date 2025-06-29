c851361e3ab2407fd29c0adda884d0b6ff85aae3
spandankvaidya <spandankvaidya@gmail.com>
2025-06-29 19:12:33 +0530

    Add app folder

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse
+from app.model import load_and_predict
+from app.dash_app import launch_dash_app
+import threading
+
+app = FastAPI()
+
+# Launch Dash app in background
+threading.Thread(target=launch_dash_app, daemon=True).start()
+
+@app.get("/run-forecast")
+def run_forecast(date: str = Query(..., regex=r"\d{4}-\d{2}-\d{2}")):
+    success = load_and_predict(date)
+    if success:
+        return JSONResponse(content={"status": "success", "date": date})
+    else:
+        return JSONResponse(content={"status": "error", "message": f"CSV not found for {date}"}, status_code=400)
